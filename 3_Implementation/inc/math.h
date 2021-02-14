51a93cdc93e9e1b4c5f4c312dd5460f1fc75f0f6
Souvik Jana <78857077+99003784@users.noreply.github.com>
2021-02-15 03:02:36 +0530

    changes made to math.h

--- 3_Implementation/inc/math.h
+++ 3_Implementation/inc/math.h
@@ -4,5 +4,10 @@
 //conversion from binary to decimal
 	int bintodec(long int a);
 
+// claculating lograrithm,exponential and power
+	double logarithm(double  x);
+	double exponent_num(double m);
+	double power( double a,double b);
+
 #endif // BTOD_H_INCLUDED
 
