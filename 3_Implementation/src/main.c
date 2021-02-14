a10f919b40cf7c52f87a54b0d969d2545d991381
99003788 <78849542+99003788@users.noreply.github.com>
2021-02-15 02:54:50 +0530

    Added main source file

--- 3_Implementation/src/main.c
+++ 3_Implementation/src/main.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+#include"math.h"
+int main()
+{
+
+     int n;
+    printf("Enter a Number:- ");
+    scanf(" %d",&n);
+        bintodec(n);
+    return 0;
+}
+
+
+
 
