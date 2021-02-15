b25646ae95f54bb8ee90860711606fa1ff4b618c
99003786 <78857588+99003786@users.noreply.github.com>
2021-02-15 09:19:27 +0530

    Create calsangam.h

---
+++ 3_Implementation/inc/calsangam.h
@@ -0,0 +1,12 @@
+#ifndef __CALSANGAM_H
+# define __CALSANGAM_H
+#include <stdio.h>
+#include <math.h>
+
+void calc();
+float sums(float san1,float san2);
+float subs(float san1,float san2);
+int mults(float san1,float san2);
+int divs(float san1,float san2);
+
+# endif
