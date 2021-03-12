d1d089d9f47a9d550db62d8275fbdec14e6fea91
Souvik Jana <78857077+99003784@users.noreply.github.com>
2021-03-12 15:12:34 +0530

    made changes in test_calculator_operations.c
    committed by 99003784

--- 3_Implementation/test/test_calculator_operations.c
+++ 3_Implementation/test/test_calculator_operations.c
@@ -18,6 +18,18 @@ void test_trigsininv(void);
 void test_trigcosinv(void);
 void test_trigtaninv(void);
 
+void test_circle(void);
+void test_rectangle(void);
+void test_square(void);
+void test_triangle(void);
+void test_cube(void);
+void test_cylinder(void);
+void test_cone(void);
+void test_sphere(void);
+
+
+
+
 /* Required by the unity test framework */
 void setUp(){}
 /* Required by the unity test framework */
@@ -41,6 +53,16 @@ int main()
   RUN_TEST(test_trigsininv);
   RUN_TEST(test_trigcosinv);
   RUN_TEST(test_trigtaninv);
+  RUN_TEST(test_circle);
+  RUN_TEST(test_rectangle);
+  RUN_TEST(test_square);
+  RUN_TEST(test_triangle);
+  RUN_TEST(test_cube);
+  RUN_TEST(test_cylinder);
+  RUN_TEST(test_cone);
+  RUN_TEST(test_sphere);
+
+  
 
   /* Close the Unity Test Framework */
   return UNITY_END();
@@ -119,3 +141,40 @@ void test_trigtaninv(void) {
   TEST_ASSERT_EQUAL(0, taninv(1));
 }
 
+// Test cases for the functions related to area of different geometrical shapes
+
+void test_circle(void)
+{
+    TEST_ASSERT_EQUAL(78.55,area_circle(5)); 
+}
+void test_rectangle(void)
+{
+    TEST_ASSERT_EQUAL(30,area_rectangle(5,6));
+}
+void test_square(void)
+{
+    TEST_ASSERT_EQUAL(144,area_square(12));
+}
+void test_triangle(void)
+{
+    TEST_ASSERT_EQUAL(6,area_triangle(4,3)); 
+}
+
+// Test cases for functions related to volume of different geometrical shapes
+
+void test_cube(void)
+{
+    TEST_ASSERT_EQUAL(125,volume_cube(5));
+}
+void test_cylinder(void)
+{
+    TEST_ASSERT_EQUAL(565.56,volume_cylinder(6,5));
+}
+void test_cone(void)
+{
+    TEST_ASSERT_EQUAL(186.63,volume_cone(6,5));
+}
+void test_sphere(void)
+{
+    TEST_ASSERT_EQUAL(267.44,volume_sphere(4));
+}
