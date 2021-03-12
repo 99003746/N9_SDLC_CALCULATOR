ee1c5a0f8539adc680fe8e858da4b111cb6fc8c7
99003786 <78857588+99003786@users.noreply.github.com>
2021-03-12 13:32:44 +0530

    Update project_main.c

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -4,9 +4,9 @@ void tearDown(void){}
 
 int main()
 {
-    int symbols;
-    float a,b,c;
-  float add(float a,float b);
+   
+float a,b,c;
+float add(float a,float b);
 float sub(float a, float b);
 float mult(float a, float b);
 float div(float a, float b);
