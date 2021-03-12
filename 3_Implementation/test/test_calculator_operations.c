ca0f6bf53b8b4a6c41345458d251ec3359b3d267
99003786 <78857588+99003786@users.noreply.github.com>
2021-03-12 12:57:15 +0530

    Update test_calculator_operations.c

--- 3_Implementation/test/test_calculator_operations.c
+++ 3_Implementation/test/test_calculator_operations.c
@@ -11,6 +11,12 @@ void test_add_testcase2(void);
 void test_subtract(void);
 void test_multiply(void);
 void test_divide(void);
+void test_trigsin(void);
+void test_trigcos(void);
+void test_trigtan(void);
+void test_trigsininv(void);
+void test_trigcosinv(void);
+void test_trigtaninv(void);
 
 /* Required by the unity test framework */
 void setUp(){}
@@ -29,6 +35,12 @@ int main()
   RUN_TEST(test_subtract);
   RUN_TEST(test_multiply);
   RUN_TEST(test_divide);
+  RUN_TEST(test_trigsin);
+  RUN_TEST(test_trigcos);
+  RUN_TEST(test_trigtan);
+  RUN_TEST(test_trigsininv);
+  RUN_TEST(test_trigcosinv);
+  RUN_TEST(test_trigtaninv);
 
   /* Close the Unity Test Framework */
   return UNITY_END();
@@ -64,3 +76,46 @@ void test_divide(void) {
   /* Dummy fail*/
   TEST_ASSERT_EQUAL(1, div(2, 2));
 }
+
+void test_trigsin(void) {
+  TEST_ASSERT_EQUAL(-0.98803162409, sine(30));
+  
+  /* Dummy fail*/
+  TEST_ASSERT_EQUAL(0.85090352453, sine(45));
+}
+
+void test_trigcos(void) {
+  TEST_ASSERT_EQUAL(0.15425144988, cos(30));
+  
+  /* Dummy fail*/
+  TEST_ASSERT_EQUAL(0.52532198881, cos(45));
+}
+
+void test_trigtan(void) {
+  TEST_ASSERT_EQUAL(-6.40533119665, tan(30));
+  
+  /* Dummy fail*/
+  TEST_ASSERT_EQUAL(1.61977519054, tan(45));
+}
+
+void test_trigsininv(void) {
+  TEST_ASSERT_EQUAL(0, sininv(0));
+  
+  /* Dummy fail*/
+  TEST_ASSERT_EQUAL(1, sininv(1));
+}
+
+void test_trigcosinv(void) {
+  TEST_ASSERT_EQUAL(1, cosinv(0));
+  
+  /* Dummy fail*/
+  TEST_ASSERT_EQUAL(0, cosinv(1));
+}
+
+void test_trigtaninv(void) {
+  TEST_ASSERT_EQUAL(0, taninv(0));
+  
+  /* Dummy fail*/
+  TEST_ASSERT_EQUAL(0, taninv(1));
+}
+
