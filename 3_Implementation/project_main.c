8a1829cc72fb68a75516106cbc6bafd52bac0564
99003786 <78857588+99003786@users.noreply.github.com>
2021-03-12 12:57:51 +0530

    Update project_main.c

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -6,39 +6,14 @@ int main()
 {
     int symbols;
     float a,b,c;
-    switch(symbols)
-  { 
-    case 1:
-    c=add(a,b);
-    break;
-    case 2:
-    c=sub(a,b);
-    break;
-    case 3:
-    c=mult(a,b);
-    break;
-    case 4:
-    c=div(a,b);
-    break;
-    case 5:
-    b=sine(a);
-    break;
-    case 6:
-    b=cosine(a);
-    break;
-    case 7:
-    b=tanf(a);
-    break;
-    case 8:
-    b=sininv(a);
-    break;
-    case 9:
-    b=cosinv(a);
-    break;
-    case 10:
-    b=taninv(a);
-    break;
-    
-    return 0;
-   } 
+  float add(float a,float b);
+float sub(float a, float b);
+float mult(float a, float b);
+float div(float a, float b);
+float sine(float a);
+float cosine(float a);
+float tanf(float a);
+float sininv(float a);
+float cosinv(float a);
+float taninv(float a);
 }
