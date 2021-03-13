e9fff441eb4b7fb0bd8d7aab2d6a091d53120a52
Souvik Jana <78857077+99003784@users.noreply.github.com>
2021-03-13 12:07:29 +0530

    made changes in project_main.c

--- 3_Implementation/project_main.c
+++ 3_Implementation/project_main.c
@@ -4,7 +4,8 @@ void tearDown(void){}
 
 int main()
 {
-   
+int breadth, length, length1, base,height,radius1,radius;
+long radius;
 float a,b,c;
 float add(float a,float b);
 float sub(float a, float b);
@@ -16,4 +17,21 @@ float tanf(float a);
 float sininv(float a);
 float cosinv(float a);
 float taninv(float a);
-}
+   
+long area_circle(long radius);
+int area_rectangle(int breadth,int length);
+int area_square(int length1);
+int area_triangle(int base,int height);
+double volume_cube(int radius1);
+int volume_cylinder(int radius,int height);
+int volume_cone(int radius,int height);
+int volume_sphere(int radius);
+   
+}  
+
+
+   
+
+
+
+
