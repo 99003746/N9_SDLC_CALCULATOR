54319e8b02f00a2269b1a41150f9e1e09a0a350f
99003787 <78854021+99003787@users.noreply.github.com>
2021-03-13 10:11:24 +0530

    Add files via upload

---
+++ 3_Implementation/inc/special_math_functions.h
@@ -0,0 +1,11 @@
+# ifndef __special_Math_functions_H_
+# define __special_Math_functions_H_
+#include<stdio.h>
+//#include "unity.h"
+
+double logarithmic(double); //function for logarithm
+double exponential(float); //function for exponent
+double squareroot(double);//function for squareroot
+double cuberoot(double);//function for cuberoot
+
+#endif
